af450b1b383ea13e03138ddafee0dfcc71739c2f
traoremoussa <moussa.traoreinfo@gmail.com>
2018-10-19 19:21:40 +0200

    ajout de version Control

---
+++ src/main/java/deqo/mtra/App.java
@@ -0,0 +1,13 @@
+package deqo.mtra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
